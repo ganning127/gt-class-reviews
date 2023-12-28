5a1d3054244acb822fbb31ee0aeb9e9a1a1a4722
Ganning Xu <ganningxu@gmail.com>
2023-12-28 01:47:55 -0500

    trying to fix timeout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,11 @@
 import Head from 'next/head';
-import clientPromise from '../lib/mongodb';
 import { Box, Heading } from '@chakra-ui/react';
-export const getServerSideProps = async () =>
-{
-  try
-  {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
 
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e)
-  {
-    return {
-      props: { isConnected: false },
-    };
-  }
-};
-
-export default function Home({
-  isConnected,
-})
+export default function Home()
 {
-  console.log(isConnected);
   return (
     <Box>
-      <Heading>Hello, {isConnected ? "true" : "false"}</Heading>
+      <Heading>Hello</Heading>
     </Box>
   );
 }
