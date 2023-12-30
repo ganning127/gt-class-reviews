d5a5c5e3a3e46c001bab423a89734a4c6a723617
Ganning Xu <ganningxu@gmail.com>
2023-12-30 10:25:49 -0500

    added color coding to explore page

--- pages/explore.jsx
+++ pages/explore.jsx
@@ -21,6 +21,7 @@ import { useRouter } from 'next/navigation';
 import { ClassCard } from '../components/ClassCard';
 import { useState } from 'react';
 import { NextSeo } from 'next-seo';
+import { numberToColorHsl, numberToColorHslWorkload } from '../lib/colorFunctions';
 
 export default function Explore({ success, classes })
 {
@@ -83,7 +84,7 @@ export default function Explore({ success, classes })
                         <Thead>
                             <Tr >
                                 <Th fontSize='sm' color="#B3A369">Class</Th>
-                                <Th isNumeric fontSize='sm' color="#B3A369"> Overall Rating</Th>
+                                <Th isNumeric fontSize='sm' color="#B3A369"> Overall</Th>
                                 <Th isNumeric fontSize='sm' color="#B3A369">Difficulty</Th>
                                 <Th isNumeric fontSize='sm' color="#B3A369">Interesting</Th>
                                 <Th isNumeric fontSize='sm' color="#B3A369">Workload</Th>
@@ -112,10 +113,23 @@ export default function Explore({ success, classes })
                                                 !c.courseName && <Td>{c.courseCode.toUpperCase()}</Td>
                                             }
 
-                                            <Td isNumeric>{c.avgOverallRating.toFixed(1)}</Td>
-                                            <Td isNumeric>{c.avgDiffRating.toFixed(1)}</Td>
-                                            <Td isNumeric>{c.avgInterestingRating.toFixed(1)}</Td>
-                                            <Td isNumeric>{c.avgWorkload.toFixed(1)}</Td>
+                                            <Td isNumeric>
+
+                                                <Text bg={numberToColorHsl(c.avgOverallRating * 10)} color={c.avgOverallRating <= 2 ? 'white' : ""} display='inline' p={1} rounded='md'>{c.avgOverallRating.toFixed(1)}</Text>
+
+                                            </Td>
+                                            <Td isNumeric>
+                                                <Text bg={numberToColorHsl((10 - c.avgDiffRating) * 10)} color={c.avgDiffRating <= 2 ? 'white' : ""} display='inline' p={1} rounded='md'>{c.avgDiffRating.toFixed(1)}</Text>
+                                            </Td>
+                                            <Td isNumeric>
+                                                <Text bg={numberToColorHsl(c.avgInterestingRating * 10)} color={c.avgInterestingRating <= 2 ? 'white' : ""} display='inline' p={1} rounded='md'>{c.avgInterestingRating.toFixed(1)}
+                                                </Text>
+                                            </Td>
+                                            <Td isNumeric>
+                                                <Text bg={numberToColorHslWorkload(c.avgWorkload)} color={c.avgWorkload >= 17 ? 'white' : ""} display='inline' p={1} rounded='md'>
+                                                    {c.avgWorkload.toFixed(1)}
+                                                </Text>
+                                            </Td>
                                             <Td isNumeric>{c.numReviews}</Td>
                                         </Tr>
                                     );
